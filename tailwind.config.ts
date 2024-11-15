946a237263b5e0f82bc54abbdb35ba752bbaedcb
pham minh khoa <khoapham280@gmail.com>
2024-11-15 22:39:20 +0700

    🎉 setup project

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    screens: {
+      sm: "480px", // Smartphone
+      md: "768px", // Tablet
+      lg: "1024px", // Laptop
+      xl: "1280px", // Monitor
+      "2xl": "1536px", // TV
+    },
+  },
+  plugins: [],
+};
+export default config;
