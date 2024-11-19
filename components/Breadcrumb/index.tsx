56e5987a5353a1a0149327a95367e4bf1641f835
pham minh khoa <khoapham280@gmail.com>
2024-11-19 11:31:42 +0700

    💄 style : responsive breadcrumb

--- components/Breadcrumb/index.tsx
+++ components/Breadcrumb/index.tsx
@@ -10,16 +10,16 @@ type Props = {
 const Breadcrumb = ({ breadcrumbs }: Props) => {
   return (
     <>
-      <div className="flex items-center gap-2 mx-auto w-3/4">
-        <IoDiamondOutline className="hover:scale-110 text-[#20475d]" />
+      <div className="flex items-center  gap-1 lg:gap-2 lg:mx-auto mx-4 lg:w-3/4">
+        <IoDiamondOutline className="hover:scale-110 text-[#20475d] h-4 w-4 lg:h-5 lg:w-5" />
         {breadcrumbs.map((breadcrumb, index) => (
           <>
             <Link href="/">
-                <TbSlash key={index} className="w-5 h-5" />
+                <TbSlash key={index} className="lg:w-5 lg:h-5 h-4 w-4" />
             </Link>
             <Link
               href={breadcrumb.url!}
-              className="hover:tracking-wider hover:text-[#20475d]"
+              className="hover:tracking-wider text-sm lg:text-base  text-[#20475d]"
             >
               {breadcrumb.title}
             </Link>
