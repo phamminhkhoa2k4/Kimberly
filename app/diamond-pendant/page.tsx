53b720c1335819afc55b95cc5a09b1ec806bba55
pham minh khoa <khoapham280@gmail.com>
2024-11-29 18:54:14 +0700

    ✨ feat : diamond pendant

--- app/diamond-pendant/page.tsx
+++ app/diamond-pendant/page.tsx
@@ -6,8 +6,13 @@ import Breadcrumb from "@/components/Breadcrumb";
 import Filter from "@/components/Filter";
 import Just from "@/components/Just";
 import ProductBanner from "@/components/productBanner";
+import useLocalStorageProducts from "@/hooks/useLocalStorageProducts";
+import { Product } from "@/types/product";
+import { useState } from "react";
 
 const DiamondPendant = () => {
+  const [rings, setRings] = useState<Product[]>();
+  const { products } = useLocalStorageProducts("products");
   return (
     <>
       <Body>
@@ -20,9 +25,16 @@ const DiamondPendant = () => {
           imageUrl="/Banner/banner-pendant.png"
           imageUrlMobile="/Banner/banner-pendant-mobile.png"
         />
-        <Filter isColor={true} isGender={true} isMaterial={true} isPrice={true} />
-        <ProductBanner />
-        <Just />
+        <Filter
+          rings={rings || []}
+          isColor={true}
+          isMaterial={true}
+          isPrice={true}
+          setRings={setRings}
+          categoryName={"Vòng Cổ"}
+        />
+        <ProductBanner rings={rings ?? []} />
+        {products.length > 0 && <Just products={products} />}
         <LastBanner />
       </Body>
     </>
